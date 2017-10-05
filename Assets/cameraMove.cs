de071ae16c2992fcc8f0f4f6559e2785dc83c6c8
ykeya.ou@gmail.com <miyavi382>
2017-10-05 15:45:24 -0400

    I am in the process of adding some blocks but this is the room the game will take place in

---
+++ Assets/cameraMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour {
+
+	public float mouseSensitivity = 100f;
+	float verticalLimit = 0f;
+	// Update is called once per frame
+	void Update () {
+		float mouseX = Input.GetAxis ("Mouse X") * Time.deltaTime * mouseSensitivity;
+		float mouseY = Input.GetAxis ("Mouse Y") * Time.deltaTime * mouseSensitivity;
+
+		//transform.Rotate (-mouseY, 0, 0); //rotate up and down for camera
+		transform.parent.Rotate(0f, mouseX, 0f); //this will rotate the cube AND the camera b/c the cmaera is parented to the cube
+
+		//new vertical looking up and down code
+		verticalLimit -= mouseY;
+		verticalLimit = Mathf.Clamp (verticalLimit, -80f, 82f);
+		//correct the camera rotation. unroll the camera.
+		transform.localEulerAngles = new Vector3 (
+			verticalLimit,
+			transform.localEulerAngles.y,
+			0f);
+
+
+		//lock the mousecursor
+		if (Input.GetMouseButtonDown (0)) {
+			Cursor.visible = false;
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+	}
+}
---
+++ Assets/playerMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour {
+	Rigidbody rb;
+	Vector3 inputVector;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float horizontalInput = Input.GetAxis ("Horizontal"); //a/d/left/right arrows
+		float verticalInput = Input.GetAxis ("Vertical"); //w/s/up/down arrows
+
+		//transform our input values based on this transforms right and forward directions
+		inputVector = transform.right * horizontalInput + transform.forward * verticalInput;
+
+		//normalizes input vector so diagonal movement isnt faster
+		if (inputVector.magnitude > 1f) {
+			inputVector = Vector3.Normalize (inputVector);
+		}
+	}
+
+	void FixedUpdate(){
+		GetComponent<Rigidbody> ().velocity = inputVector * 25f + Physics.gravity * .9f;
+	}
+}
